e046aa09157c7f53f2cb382e8679d057e6888af1
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-03-19 01:13:30 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,7 +3,7 @@ function Hero() {
     <section className=" pt-8  pb-[4rem] ">
       <div className="container-wrapper">
         <h1 className=" font-groot-one text-2xl text-center sm:text-5xl font-semibold my-4 max-w-full sm:max-w-5xl mx-auto text-[#000]">
-          SpongeSol is all about Memes, Cuteness, and Solana Jungle Adventure!
+        SpongSol: Making Waves in the Solana Sea with Memes!
         </h1>
         <div className="relative z-50">
           <div className=" flex justify-between flex-col sm:flex-row items-center ">
@@ -13,12 +13,7 @@ function Hero() {
 
             <div className=" max-w-2xl bg-[#42e2e7c5] py-8 px-4 rounded-3xl border-2 border-[#ffc93b]">
               <h2 className=" text-lg text-black font-semibold">
-                Meet SpongeSol: the next meme sensation in the wild world of
-                crypto. Think you've seen it all with Shiba, Baby Doge, and our
-                dear froggy friend Pepe? Think again. Here comes a feline with
-                rhythm, ambition, and a dream to join the meme coin hall of
-                fame. Why should dogs and frogs have all the fun? Introducing
-                SpongeSol 
+              Welcome to SpongeSol, the most bubbly and optimistic meme coin on Solana! Just like SpongeBob, we're all about fun, friendship, and making waves in the crypto world. Join us in Bikini Bottom for meme creation, staking, and endless water adventures!
               </h2>
               <div className=" py-2">
                 <a
